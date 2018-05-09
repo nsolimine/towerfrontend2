51359d690d1c8868a673693955c1039e8418d4e3
nsolimine <32491766+nsolimine@users.noreply.github.com>
2018-05-09 14:41:49 -0600

    now we're jammin, the UI is looking better

--- src/beginner.js
+++ src/beginner.js
@@ -22,20 +22,16 @@ export class Section1 extends React.Component {
       this.loadData()
     }
 
-
-
-
-
     updateSongBeginner = (event) => {
-      this.props.updateSongBeginner(this.state.item)
+      this.props.updateSongBeginner(this.state.beginner)
     }
 
     deleteSong = (event) => {
-      this.props.deleteSong(this.state.item)
+      this.props.deleteSong(this.state.beginner)
     }
 
     deleteSongBeginner = (level, id) => {
-      return fetch('https://towerbackend.herokuapp.com/' + level + '/' + id,  {
+      return fetch("https://towerbackend.herokuapp.com/beginner/" + id,  {
         method: 'DELETE',
         headers: new Headers({
           'Content-Type': 'application/json',
@@ -67,34 +63,19 @@ export class Section1 extends React.Component {
       }
     }
 
-    toggleFunction = (item) => {
-      const { togglePanels } = this.state
-      const index = togglePanels.indexOf(item.id)
-      if(index !== -1){
-        togglePanels.splice(index, 1)
-      } else {
-        togglePanels.push(item.id)
-      }
-      this.setState({ togglePanels })
-    }
-
   createListItemBeginner(item){
     return (
       <li key={item.id}>
         <div className="profile">
-          <h4 className="profileLevel">Level Up: {item.artist}</h4>
+          <h4 className="profileLevel">{item.song}</h4>
         </div>
         <div>
+          <p>"{item.song}"</p>
           <p>Difficulty: {item.difficulty}</p>
           <p>Artist: {item.artist}</p>
-          <p>Song: "{item.song}"</p>
-          <p>Techniques Used: {item.technique}</p>
+          <p>Techniques: {item.technique}</p>
           <p><a href={item.url} target="blank">Listen on YouTube</a></p>
           <p><a href={item.tabUrl} target="blank">Check out the tab!</a></p>
-          {/* <div className="buttons">
-              {this.renderUpdateButton(item)}
-              {this.renderDeleteButton(item)}
-          </div> */}
         </div>
       </li>
     );
